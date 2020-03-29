40b99717e7419d0eeb47b0ad28fa67b452b6034e
Waseem Hassan <waseemh40@gmail.com>
2020-03-29 15:09:08 +0200

    In corona time, version for mean, max and min temp and humi values of last day and day count, for iA batch 2 of lettuce. Not tested or progammed

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include "segmentlcd.h"
 
-
+#define 	AVERAGE_SAMPLES 48		//per 30 minutes
 
 
 void 	inc_dec_irq_handler(uint8_t irq_number);
@@ -26,23 +26,34 @@ typedef enum {	g_config=0,
 				b_config,
 				cycle,
 				hours_offset,
+				days_offset,
+				temp_humi,
 				display
 			}menu_mode_t;
 typedef enum {	hrs=0,
 				min,
 				temp,
-				humi
+				humi,
+				days
 			}time_data_t;
+typedef enum {	temp_avg=0,
+				temp_max,
+				temp_min,
+				humi_avg,
+				humi_max,
+				humi_min
+			}temp_humi_disp_t;
 
-			menu_mode_t menu_mode=display,last_menu_mode=display;
-			uint8_t		sub_menu=0;
-			uint8_t		g_inten=G_INTEN_DEF,r_inten=R_INTEN_DEF,b_inten=B_INTEN_DEF;		//default values
-			bool		right_pressed=false;
-			time_data_t	time_data=temp;		//display temp by default
+			menu_mode_t 		menu_mode=display,last_menu_mode=display;
+			uint8_t				g_inten=G_INTEN_DEF,r_inten=R_INTEN_DEF,b_inten=B_INTEN_DEF;		//default values
+			bool				right_pressed=false;
+			time_data_t			time_data=temp;					//display temp by default
+			temp_humi_disp_t	temp_humi_disp=temp_avg;		//display avg. temp by default
 				//time related
 volatile	uint8_t		sample_counter=0;
 volatile	uint64_t	seconds=0;
 volatile	uint8_t		mins=0,hours=0;
+volatile	uint8_t		days_count=0;
 volatile	uint8_t		running_cycle=12;		//default value of 12 hrs
 
 volatile	dht_data_t 	dht_data;
@@ -50,6 +61,19 @@ volatile	dht_data_t 	dht_data;
 			uint32_t	hum_val=0;
 			uint32_t	temp_val=0;
 
+			//average, max. & min. temp. and humidity related parameters
+volatile	uint32_t	temp_avg_c=0;
+volatile	uint32_t	temp_min_c=0;
+volatile	uint32_t	temp_max_c=0;
+volatile	uint32_t	humi_avg_c=0;
+volatile	uint32_t	humi_min_c=0;
+volatile	uint32_t	humi_max_c=0;
+volatile	uint32_t	temp_avg_d=0;
+volatile	uint32_t	temp_min_d=0;
+volatile	uint32_t	temp_max_d=0;
+volatile	uint32_t	humi_avg_d=0;
+volatile	uint32_t	humi_min_d=0;
+volatile	uint32_t	humi_max_d=0;
 
 
 			char 		rs232_buf[128];
@@ -140,6 +164,21 @@ int main(void)
 									hours=0;
 									rgb_pixel=convert_inten_pixel(g_inten,r_inten,b_inten);
 									chalo_batti( rgb_pixel);
+									days_count++;
+										//update temp. & humi. display values
+									temp_avg_d=(uint32_t)(temp_avg_c/AVERAGE_SAMPLES);
+									temp_max_d=(uint32_t)(temp_max_c);
+									temp_min_d=(uint32_t)(temp_min_c);
+									temp_avg_c=0;
+									temp_max_c=0;
+									temp_min_c=0;
+									humi_avg_d=(uint32_t)(humi_avg_c/AVERAGE_SAMPLES);
+									humi_max_d=(uint32_t)(humi_max_c);
+									humi_min_d=(uint32_t)(humi_min_c);
+									humi_avg_c=0;
+									humi_max_c=0;
+									humi_min_c=0;
+
 								}
 								if(hours>=running_cycle){
 									rgb_pixel=convert_inten_pixel(G_INTEN_MIN,R_INTEN_MIN,B_INTEN_MIN);
@@ -164,6 +203,26 @@ int main(void)
 							sprintf(rs232_buf,"Pixel mode=%2x\n",menu_mode);
 							debug_str(rs232_buf);
 							read_dht=false;
+							//avergae related
+							if(seconds==1800){		//30 minutes sampling
+									//temperature related
+								temp_avg_c+=temp_val;
+								if(temp_min_c>temp_val){
+									temp_min_c=temp_val;
+								}
+								if(temp_max_c<temp_val){
+									temp_max_c=temp_val;
+								}
+									//temperature related
+								humi_avg_c+=hum_val;
+								if(humi_min_c>hum_val){
+									humi_min_c=hum_val;
+								}
+								if(humi_max_c<hum_val){
+									humi_max_c=hum_val;
+								}
+
+							}
 						}
 							//menu related update @ 10 Hz
 						if(right_pressed || last_menu_mode!=menu_mode || seconds%60==0){
@@ -231,8 +290,8 @@ void menu_system(void){
 			SegmentLCD_AllOff();
 			if(right_pressed){
 				time_data++;
-				if(time_data>3){
-					time_data=0;
+				if(time_data>days){
+					time_data=hrs;
 				}
 				right_pressed=false;
 			}
@@ -255,6 +314,11 @@ void menu_system(void){
 				sprintf((const char *)lcd_buf,"Humi");
 				SegmentLCD_Write((const char *)lcd_buf);
 				SegmentLCD_Number(hum_val);
+			}
+			else if(time_data==days){
+				sprintf((const char *)lcd_buf,"Days");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(days_count);
 			}
 			else{
 				SegmentLCD_Write((const char *)"Invalid");
@@ -291,6 +355,60 @@ void menu_system(void){
 			sprintf((const char *)lcd_buf,"Hrs_off.");
 			SegmentLCD_Write((const char *)lcd_buf);
 			SegmentLCD_Number(hours);
+			break;
+		case days_offset:
+			if(right_pressed){
+				days_count+=1;
+				right_pressed=false;
+			}
+			SegmentLCD_AllOff();
+			sprintf((const char *)lcd_buf,"Days_off.");
+			SegmentLCD_Write((const char *)lcd_buf);
+			SegmentLCD_Number(days_count);
+			break;
+		case temp_humi:
+			SegmentLCD_AllOff();
+			if(right_pressed){
+				temp_humi_disp++;
+				if(temp_humi_disp>humi_min){
+					temp_humi_disp=temp_avg;
+				}
+				right_pressed=false;
+			}
+			if(temp_humi_disp==temp_avg){
+				sprintf((const char *)lcd_buf,"T_AVG");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(temp_avg_d);
+			}
+			else if(temp_humi_disp==temp_max){
+				sprintf((const char *)lcd_buf,"T_MAX");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(temp_max_d);
+			}
+			else if(temp_humi_disp==temp_min){
+				sprintf((const char *)lcd_buf,"T_MIN");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(temp_min_d);
+			}
+			else if(temp_humi_disp==humi_avg){
+				sprintf((const char *)lcd_buf,"H_AVG");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(humi_avg_d);
+			}
+			else if(temp_humi_disp==humi_max){
+				sprintf((const char *)lcd_buf,"H_MAX");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(humi_max_d);
+			}
+			else if(temp_humi_disp==humi_min){
+				sprintf((const char *)lcd_buf,"H_MIN");
+				SegmentLCD_Write((const char *)lcd_buf);
+				SegmentLCD_Number(humi_min_d);
+			}
+			else{
+				SegmentLCD_Write((const char *)"Invalid");
+				temp_humi_disp=0;
+			}
 			break;
 		default:
 			menu_mode=display;
